420ed0baf034dd76ed394e96a058347fd1ba0649
Andy Schachter <andy.schachter@gmail.com>
2021-03-10 20:39:05 -0500

    Updated Calculator for all 4 basic math functions

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-function calculate() {
+function calculate(pemdas) {
     
-    var number1 = parseFloat(document.getElementById("fnumber").value);
-    var number2 = parseFloat(document.getElementById("snumber").value);
-    var result = number1 + number2;
-    
-    document.getElementById("addresult").value = result;
+    var number1 = parseFloat(document.getElementById("first-number").value);
+    var number2 = parseFloat(document.getElementById("second-number").value);
+    // var pemdas = document.getElementByTagName("button").value;
+    var result;
+    console.log(pemdas)
+    switch (pemdas) {
+        case "Add!":
+            result = number1 + number2;
+            break;
+        case "Subtract!":
+            result = number1 - number2;
+            break;
+        case "Multiply!":
+            result = number1 * number2;
+            break;
+        case "Divide!":
+            result = number1 / number2;
+            break;
+        case "Reset":
+            result = "";
+            number1 = "";
+            number2 = "";
+            break;
+        default:
+            result = "";
+        }
+    console.log (result) 
+    document.getElementById("result").value = result
+    document.getElementById("first-number").value = number1
+    document.getElementById("second-number").value = number2
+        // = number1 + number2;
 }
